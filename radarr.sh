be431bc70fce04d3841ab06995794098d4562f8f
Sayem Chowdhury <sayem314@gmail.com>
2017-11-03 18:07:27 +0600

    Create radarr.sh

---
+++ radarr.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# radarr-installer by @sayem314
+#
+
+# Global value
+user="mediaserver"
+installdir="/opt/$user"
+
+# install mono if not exist
+hash mono 2>/dev/nul || wget https://raw.githubusercontent.com/sayem314/pirates-mediaserver/master/mono.sh -O - -o /dev/null|bash
+
+# Creating non-root user
+[[ -d $installdir ]] || mkdir -p $installdir
+echo "Creating user '$user'"
+if id -u $user >/dev/null 2>&1; then
+	echo "User '$user' already exists. Skipped!"
+else
+	useradd -r -d $installdir -s /bin/false $user
+	chown -R $user:$user $installdir
+fi
+
+# working directory
+cd $installdir || exit
+
+echo "Installing radarr"
+wget $( wget -qO- https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
+tar -xzf Radarr.develop.*.linux.tar.gz
+rm -f Radarr.develop.*.linux.tar.gz
+chown -R $user:$user $installdir/Radarr
+
+# Create startup service
+init=$(cat /proc/1/comm)
+if [[ "$init" == "systemd" ]]; then
+	echo "Creating systemd service"
+	echo "[Unit]
+Description=Radrr Daemon
+After=network.target
+
+[Service]
+WorkingDirectory=$installdir/Radarr
+Type=simple
+User=$user
+ExecStart=/usr/bin/mono Radarr.exe --NoRestart
+Restart=always
+RestartSec=2
+TimeoutStopSec=5
+
+[Install]
+WantedBy=multi-user.target
+"> /etc/systemd/system/radarr.service
+	chmod 0644 /etc/systemd/system/radarr.service
+	systemctl daemon-reload
+	systemctl enable radarr
+	service radarr start
+fi
+
+echo "Install finished. Default port is 7878"
