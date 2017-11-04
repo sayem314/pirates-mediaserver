92c88be13d332582ec5d965de4458ef8de462a26
Sayem Chowdhury <sayem314@gmail.com>
2017-11-04 19:32:36 +0600

    support ubuntu

--- mono/mono-install.sh
+++ mono/mono-install.sh
@@ -8,18 +8,14 @@ echo "Installing mono"
 if hash apt-get 2>/dev/null; then
 	apt-get install lsb-release curl dirmngr -y
 	CODENAME=$(cat /etc/*-release | grep "VERSION_ID=" | cut -f1 -d'.'| cut -f2 -d'"')
-	DEBIAN=$(lsb_release -i | awk 'NF{ print $NF }')
-	if [[ $DEBIAN == Debian ]]; then
-		if [[ $CODENAME -ge 7 ]]; then
-			RELEASENAME=$(lsb_release -c | awk 'NF{ print $NF }')
-			apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-			echo "deb http://download.mono-project.com/repo/debian $RELEASENAME main" | tee /etc/apt/sources.list.d/mono-official.list
-			apt-get update
-			apt-get install mono-devel -y
-			apt-get install libmono-cil-dev mediainfo sqlite3 -y
-		else
-			echo "Only debian is supported at this moment"
-		fi
+	OSNAME=$(lsb_release -i | awk 'NF{ print $NF }' | tr '[:upper:]' '[:lower:]')
+	if [[ $CODENAME -ge 7 ]]; then
+		RELEASENAME=$(lsb_release -c | awk 'NF{ print $NF }')
+		apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+		echo "deb http://download.mono-project.com/repo/$OSNAME $RELEASENAME main" | tee /etc/apt/sources.list.d/mono-official.list
+		apt-get update
+		apt-get install mono-devel -y
+		apt-get install libmono-cil-dev mediainfo sqlite3 -y
 	fi
 	echo "Install finished."
 else
