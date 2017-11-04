a13592607671dac33f1f08dee72ed6c8564d4d5c
Sayem Chowdhury <sayem314@gmail.com>
2017-11-04 16:31:22 +0600

    Create jackett-update.sh

---
+++ jackett/jackett-update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# jackett-updater by @sayem314
+#
+
+# Global value
+user="mediaserver"
+installdir="/opt/$user"
+
+# working directory
+cd $installdir || exit
+
+# stop jackett first
+service jackett stop
+
+echo "Updating jackett"
+wget -q $( wget -qO- https://api.github.com/repos/Jackett/Jackett/releases | grep Jackett.Binaries.Mono.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
+rm -rf Jackett
+tar -xzf Jackett.Binaries.Mono.tar.gz
+rm -f Jackett.Binaries.Mono.tar.gz
+chown -R $user:$user Jackett
+
+# start jackett now
+service jackett start
+
+echo "Update finished."
