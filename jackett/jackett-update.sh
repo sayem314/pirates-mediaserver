3d22b317e761898a109007b8fe481fc7b442cea9
Sayem Chowdhury <sayem314@gmail.com>
2017-11-04 17:48:33 +0600

    Update jackett-update.sh

--- jackett/jackett-update.sh
+++ jackett/jackett-update.sh
@@ -14,7 +14,7 @@ cd $installdir || exit
 service jackett stop
 
 echo "Updating jackett"
-wget -q $( wget -qO- https://api.github.com/repos/Jackett/Jackett/releases | grep Jackett.Binaries.Mono.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
+wget -q "$( wget -qO- https://api.github.com/repos/Jackett/Jackett/releases | grep Jackett.Binaries.Mono.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )"
 rm -rf Jackett
 tar -xzf Jackett.Binaries.Mono.tar.gz
 rm -f Jackett.Binaries.Mono.tar.gz
