8dc7c16390f24dad26b267fc195eb18add19bab5
Sayem Chowdhury <sayem314@gmail.com>
2017-11-04 16:40:05 +0600

    Create radarr-update.sh

---
+++ radarr/radarr-update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# radarr-updater by @sayem314
+#
+
+# Global value
+user="mediaserver"
+installdir="/opt/$user"
+
+# working directory
+cd $installdir || exit
+
+# stop radarr first
+service radarr stop
+
+echo "Updating radarr"
+wget -q $( wget -qO- https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
+tar -xzf Radarr.develop.*.linux.tar.gz
+rm -f Radarr.develop.*.linux.tar.gz
+chown -R $user:$user Radarr
+
+# start radarr now
+service radarr start
+
+echo "Update finished."
