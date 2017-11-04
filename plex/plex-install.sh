a15aae8730902f2c28fdb6afe793a6844ec9eb75
Sayem Chowdhury <sayem314@gmail.com>
2017-11-04 21:03:09 +0600

    Add files via upload

--- plex/plex-install.sh
+++ plex/plex-install.sh
@@ -7,10 +7,10 @@ if hash apt-get 2>/dev/null; then
 	# Detect architecture. We only support linux.
 	if uname -m | grep -q 64; then
 		wget -q https://downloads.plex.tv/plex-media-server/1.9.4.4325-1bf240a65/plexmediaserver_1.9.4.4325-1bf240a65_amd64.deb
-
 	elif uname -m | grep -q 86; then
 		wget -q https://downloads.plex.tv/plex-media-server/1.9.4.4325-1bf240a65/plexmediaserver_1.9.4.4325-1bf240a65_i386.deb
 	fi
+
 	dpkg -i plexmediaserver*.deb
 	rm -f plexmediaserver*.deb
 	mkdir -p /var/plex/media
@@ -22,8 +22,8 @@ elif hash yum 2>/dev/null; then
 		wget -q https://downloads.plex.tv/plex-media-server/1.9.4.4325-1bf240a65/plexmediaserver-1.9.4.4325-1bf240a65.x86_64.rpm
 	elif uname -m | grep -q 86; then
 		wget -q https://downloads.plex.tv/plex-media-server/1.9.4.4325-1bf240a65/plexmediaserver-1.9.4.4325-1bf240a65.i386.rpm
-
 	fi
+
 	yum install plexmediaserver*.rpm
 	rm -f yum install plexmediaserver*.rpm
 	systemctl enable plexmediaserver.service
